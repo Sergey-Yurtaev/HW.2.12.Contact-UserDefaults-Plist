56ee68db2392a6c63bf3b2569977e46a03e0d877
Sergey-Yurtaev <engineerk2@mail.ru>
2022-05-31 14:47:17 +0500

    Initial Commit

---
+++ HW.2.12.Contact(UserDefaults)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.12.Contact(UserDefaults)
+//
+//  Created by Sergey Yurtaev on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
